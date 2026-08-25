a5ca41d28c5ef28776d4add32261f00566a8aa01
Syth-00 <smbjacky@gmail.com>
2026-08-25 11:28:58 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-console.log("Luna Dashboard Loaded");
+const loginForm = document.querySelector("form");
+
+loginForm.addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+    const username = document.querySelector("#username").value;
+    const password = document.querySelector("#password").value;
+
+    alert(
+        "Username: " + username +
+        "\nPassword: " + password
+    );
+
+});
